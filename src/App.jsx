e9178f090292050a2b521821bd1722d3c2499292
Javier Barreto <javierbarretocp@gmail.com>
2024-03-14 15:21:02 -0600

    preparing for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
       <Navbar />
       <div className="d-flex justify-content-center">
         <h1>Website content</h1>
-        <button type="button" className="btn btn-secondary" onClick={() => console.log(isLogged())}>Check login status</button>
+        {/* <button type="button" className="btn btn-secondary" onClick={() => console.log(isLogged())}>Check login status</button> */}
       </div>
     </div>
   )
--- src/javascript/firestore/index.js
+++ src/javascript/firestore/index.js
@@ -1,22 +1,24 @@
 import { app } from '../../../db/db'
-import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore"
+import { doc, getFirestore, setDoc } from "firebase/firestore"
 import { getUserId } from '../userActions';
 
 const db = getFirestore(app)
-const referencia = doc(db, "userTasks");
+// const referencia = doc(db, "userTasks");
 
 const createUserTasksDB = async (uid) => {
-  await setDoc(doc(db, "userTasks", "user-" + uid), { // db - Colleccion - Nombre del documento
+  await setDoc(doc(db, "userTasks", `user-${uid}`), { // db - Colleccion - Nombre del documento
     tasks: {}
   });
 }
 
-const getUserTasks = async () => {
-  const uid = getUserId()
-  const referencia = doc(db, "userTasks", `user-${uid}`);
-  const data = await getDoc(referencia);
 
-  console.log(data)
-}
+// TODO: Terminar el get info y agregar funcionalidad de borrar, modificar. Ademas, agregar localstorage
+// const getUserTasks = async () => {
+//   const uid = getUserId()
+//   const referencia = doc(db, "userTasks", `user-${uid}`);
+//   const data = await getDoc(referencia);
+
+//   console.log(data)
+// }
 
 export { createUserTasksDB }
